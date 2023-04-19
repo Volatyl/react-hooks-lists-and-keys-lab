99be862c979ffdfb2dfc40d090ddd19c0c9f24f7
Kennedy Muinde <kennedymuinde@gmail.com>
2023-04-19 07:35:50 +0300

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,14 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const link = links.map((link) => (
+    <a href={`#${link}`} key={link}>
+      {" "}
+      {link}{" "}
+    </a>
+  ));
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{link}</nav>;
 }
 
 export default NavBar;
